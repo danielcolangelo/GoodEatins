f6933ac505b4af55a25cebba1500132a9fb7c5fb
Daniel Colangelo <danalfons@yahoo.com>
2019-06-10 21:48:44 -0400

    just started
    started working on the UI and added the assets.

---
+++ GoodEatins/GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Danny on 6/10/19.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title : String
+    let imageName : String
+}
---
+++ GoodEatins/GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Danny on 6/10/19.
+//  Copyright © 2019 Danny. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title : String
+    var instructions : String
+    var imageName : String
+}
